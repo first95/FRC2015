81a59d13668e2beccc9f7d0e179387b7adbefbd2
laptop2014 <laptop2014@localhost>
2015-01-06 18:43:04 -0500

    Made initial guesses as to what will be required.

--- FRC2015/src/edu/wpi/first/wpilibj/templates/Robot2015.java
+++ FRC2015/src/edu/wpi/first/wpilibj/templates/Robot2015.java
@@ -9,6 +9,10 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Gyro;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,11 +22,28 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot2015 extends IterativeRobot {
+    Talon frontLeft, frontRight, backLeft, backRight;
+    RobotDrive driveTrain;
+    
+    Gyro gyro;
+    
+    Joystick chasis, weapons;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+        frontLeft = new Talon(1);
+        frontRight = new Talon(2);
+        backLeft = new Talon(3);
+        backRight = new Talon(4);
+        driveTrain = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+        
+        gyro = new Gyro(1);
+        
+        chasis = new Joystick(1);
+        weapons = new Joystick(2);
 
     }
 
@@ -37,7 +58,13 @@ public class Robot2015 extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        double x, y, rotate, turned;
+        x = chasis.getRawAxis(1);
+        y = chasis.getRawAxis(3);
+        rotate = chasis.getRawAxis(2);
+        turned = gyro.getAngle();
         
+        driveTrain.mecanumDrive_Cartesian(x, y, rotate, turned);
     }
     
     /**
