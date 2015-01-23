e5d58e2a69d45d7a5376669a8b0c1102f98bbfe2
Spencer <Spencer@localhost>
2015-01-22 20:11:09 -0500

    resetable gyro

--- FRC2015/src/org/usfirst/frc/team95/robot/ResetableGyro.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/ResetableGyro.java
@@ -4,15 +4,26 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Gyro;
 
 public class ResetableGyro extends Gyro {
+	double offset;
 	
 
 	public ResetableGyro(AnalogInput channel) {
 		super(channel);
+		offset = 0;
 		// TODO Auto-generated constructor stub
 	}
 	
 	public ResetableGyro(int channel) {
 		super(channel);
+		offset = 0;
+	}
+	
+	public void set(double thatway) {
+		offset = thatway - super.getAngle();
+	}
+	
+	public double getAngle; {
+		return super.getAngle() + offset;
 	}
 
 }
