5b4a4fa0d68c58abda33cbe7352553a3520d194c
daroc <daroc@localhost>
2015-01-24 12:15:41 -0500

    Grab golden totes.

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GrabGoldenTotes.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import java.util.Vector;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+public class GrabGoldenTotes extends AutoMove {
+	Robot robot;
+	Vector<AutoMove> vector;
+	SequentialMove sequential;
+	
+	public GrabGoldenTotes(Robot robo) {
+		robot = robo;
+		vector = new Vector<AutoMove>();
+		vector.add(new TakeToteRight(robot));
+		vector.add(new TakeToteRight(robot));
+		vector.add(new TakeToteRight(robot));
+		vector.add(new GoBack(robot));
+		sequential = new SequentialMove(vector);
+	}
+
+	@Override
+	public Status init() {
+		return sequential.init();
+	}
+
+	@Override
+	public Status periodic() {
+		return sequential.periodic();
+	}
+
+	@Override
+	public Status stop() {
+		return sequential.stop();
+	}
+
+}
