67a9f3d516c1018216e6e6a38bdbc5ec8eaacba0
Spencer <Spencer@localhost>
2015-01-24 12:16:21 -0500

    Go Forwar and backward

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GoForward.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import java.util.Vector;
+import org.usfirst.frc.team95.robot.Robot;
+
+public class GoForward extends AutoMove {
+	Robot robot;
+	AutoMove sequence;
+	
+	public GoForward(Robot robo) {
+		robot = robo;
+		MoveRelative move;
+		move = new MoveRelative(robot, 0.0, 0.75, 0.0, 2.0);
+		Vector<AutoMove> vector;
+		vector = new Vector<AutoMove>();
+		vector.add(move);
+		sequence = new SequentialMove(vector);
+	}
+
+	@Override
+	public Status init() {
+		return sequence.init();
+	}
+
+	@Override
+	public Status periodic() {
+		return sequence.periodic();
+	}
+
+	@Override
+	public Status stop() {
+		return sequence.stop();
+	}
+
+}
