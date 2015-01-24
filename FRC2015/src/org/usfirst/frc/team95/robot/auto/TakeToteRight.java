10e585b17c5792169f92dfe31bb08f5f1d081b55
Spencer <Spencer@localhost>
2015-01-24 11:48:27 -0500

    move right w/ tote

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/TakeToteRight.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/TakeToteRight.java
@@ -1,5 +1,39 @@
 package org.usfirst.frc.team95.robot.auto;
 
-public class TakeToteRight {
+import java.util.Vector;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+public class TakeToteRight extends AutoMove {
+	Robot robot;
+	AutoMove sequence;
+	
+	public TakeToteRight(Robot robo) {
+		robot = robo;
+		PickUpTote tote;
+		tote = new PickUpTote(robot, null);
+		MoveRelative move;
+		move = new MoveRelative(robot, 0.75, 0.0, 0.0, 2.0);
+		Vector<AutoMove> vector;
+		vector = new Vector<AutoMove>();
+		vector.add(tote);
+		vector.add(move);
+		sequence = new SequentialMove(vector);
+	}
+
+	@Override
+	public Status init() {
+		return sequence.init();
+	}
+
+	@Override
+	public Status periodic() {
+		return sequence.periodic();
+	}
+
+	@Override
+	public Status stop() {
+		return sequence.stop();
+	}
 
 }
