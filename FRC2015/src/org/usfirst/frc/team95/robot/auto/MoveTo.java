c6fc259b3da45012b021f1615ed2942dfe3b45a0
daroc <daroc@localhost>
2015-01-24 10:26:52 -0500

    Saving overcomplicated changes for posterity.

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/MoveTo.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/MoveTo.java
@@ -1,17 +1,19 @@
 package org.usfirst.frc.team95.robot.auto;
 
+import org.usfirst.frc.team95.robot.PIDMecanumOutput;
 import org.usfirst.frc.team95.robot.Robot;
 
 public class MoveTo extends AutoMove {
 
 	Robot robot;
+	PIDMecanumOutput xPID, yPID, rotationPID;
 	double targetX, targetY, targetRotation;
 	
 	public MoveTo(Robot robo, double x, double y, double rotation) {
 		robot = robo;
 		targetX = x;
 		targetY = y;
-		targetRotation = rotation
+		targetRotation = rotation;
 	}
 	
 	public Status init() {
