9a81fdc0c48c2963008afcd3ee6d7f87d62a3ffe
Spencer <Spencer@localhost>
2015-01-24 10:38:11 -0500

    pick up tote auto

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/PickUpTote.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/PickUpTote.java
@@ -1,17 +1,34 @@
 package org.usfirst.frc.team95.robot.auto;
 
 import org.usfirst.frc.team95.robot.Robot;
-import org.usfirst.frc.team95.robot.auto.AutoMove.Status;
+import edu.wpi.first.wpilibj.PIDController;
 
 public class PickUpTote extends AutoMove {
 	
 	Robot robot;
+	PIDController fingerController;
 	
-	public PickUpTote(Robot robo){
+	public PickUpTote(Robot robo, PIDController fingers){
 		robot = robo;
+		fingerController = robo.fingerController;
 	}
 	
 	public Status init() {
+		fingerController.setSetpoint(fingerController.get() + 1);
 		return Status.wantsToContinue;
 	}
+
+	public Status periodic() {
+		if (fingerController.onTarget()) {
+			return Status.isNotAbleToContinue;
+		}
+		else {
+			return Status.needsToContinue;
+		}
+	}
+
+	public Status stop() {
+		return Status.isNotAbleToContinue;
+	}
+		
 }
