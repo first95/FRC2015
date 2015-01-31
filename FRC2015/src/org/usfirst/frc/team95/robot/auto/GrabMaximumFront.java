13ce260dfa76a3673c3d857dd8720e393def3ef5
daroc <daroc@localhost>
2015-01-31 10:43:17 -0500

    Added maximum front move.

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GrabMaximumFront.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+/**
+ * Grabs one can, and all three golden totes, before sliding back into the Auto zone.
+ * @author daroc
+ *
+ */
+
+public class GrabMaximumFront extends PureSequentialMove {
+	
+	public GrabMaximumFront(Robot robot) {
+		AutoMove[] moves = {new PickUpTote(robot), new MoveRelative(robot, -0.75, 0, 0, 0.5),
+				new PickUpCan(robot), new MoveRelative(robot, -0.75, 0, 0, 1.5),
+				new PickUpTote(robot), new MoveRelative(robot, -0.75, 0, 0, 2),
+				new PickUpTote(robot), new MoveRelative(robot, 0, -0.75, 0, 2),
+				new NoMove(robot)};
+		sequence = new SequentialMove(moves);
+	}
+
+}
