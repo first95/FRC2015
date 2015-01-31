f86bd29623d05b355d064d2b610b8b5b6728bd13
laptop2014 <laptop2014@localhost>
2015-01-30 19:09:41 -0500

    working plybot

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GoBackward.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team95.robot.auto;
+
+/**
+ * Move directly backwards
+ */
+
+import org.usfirst.frc.team95.robot.Robot;
+
+public class GoBackward extends PureSequentialMove {
+
+	public GoBackward(Robot robot) {
+		AutoMove[] vector = { new MoveRelative(robot, 0.0, -0.75, 0.0, 2.0) };
+		sequence = new SequentialMove(vector);
+	}
+
+}
