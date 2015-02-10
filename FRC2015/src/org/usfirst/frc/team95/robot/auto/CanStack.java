ba2a4827d22188f8ba07de481ba1a8aa8d30023f
Spencer <Spencer@localhost>
2015-02-10 17:57:12 -0500

    trying to merge

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/CanStack.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.Robot;
+import org.usfirst.frc.team95.robot.RobotConstants;
+import edu.wpi.first.wpilibj.Timer;
+
+public class CanStack extends AutoMove {
+	Robot robot;
+	int stage = 0;
+
+	public CanStack(Robot robo) {
+		robot = robo;
+	}
+
+	@Override
+	public Status init() {
+		
+	}
+
+	@Override
+	public Status periodic() {
+	
+	}
+
+	@Override
+	public Status stop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
