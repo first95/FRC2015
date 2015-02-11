59aba3aee615090a80d2ffd17e1862946373049d
daroc <daroc@localhost>
2015-02-11 17:47:17 -0500

    Testing

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/SyncGroup.java
@@ -0,0 +1,67 @@
+/*
+ * This File is released under the LGPL.
+ * You may modify this software, use it in a project, and so on,
+ * as long as this header remains intact.
+ */
+
+package org.usfirst.frc.team95.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.PIDOutput;
+
+/**
+ *
+ * @author daroc
+ */
+public class SyncGroup implements PIDOutput {
+    SpeedController[] mSpeedControllers;
+    boolean[] mReversed;
+    double maxSpeed = Math.PI / 4;
+    double minSpeed = -Math.PI / 4;
+    
+    
+    public SyncGroup (SpeedController[] SpeedControllers) {
+        mSpeedControllers = SpeedControllers;
+        mReversed = new boolean[mSpeedControllers.length]; //Initializes to false
+        System.out.println("I'm initializing.");
+    }
+    
+    public SyncGroup (SpeedController[] SpeedControllers, boolean[] Reversed) {
+        mSpeedControllers = SpeedControllers;
+        mReversed = Reversed;
+    }
+    
+    
+    public void set(double d) {
+    	if (d > maxSpeed) {
+    		d = maxSpeed;
+    	} else if (d < minSpeed) {
+    		d = minSpeed;
+    	}
+    	
+        for (int i = 0; i < mSpeedControllers.length;  i++) {
+            if (mReversed[i]) {
+                mSpeedControllers[i].set(-d);
+            } else {
+                mSpeedControllers[i].set(d);
+            }
+        }
+    }
+    
+    public void pidWrite(double bob) {
+    	set(bob);
+    }
+    
+    public double getMotor() {
+        return mSpeedControllers[0].get();
+    }
+    
+    public void setMaxSpeed(double speed) {
+    	maxSpeed = speed;
+    }
+    
+    public void setMinSpeed(double speed) {
+    	minSpeed = speed;
+    }
+    
+}
