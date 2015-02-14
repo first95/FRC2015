c41a5faaac0ab29932d8eac7866ee922a6c7dd56
Spencer <Spencer@localhost>
2015-02-14 13:02:36 -0500

    can auto stuff

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/SyncGroup.java
@@ -0,0 +1,78 @@
+/*
+ * This File is released under the LGPL.
+ * You may modify this software, use it in a project, and so on,
+ * as long as this header remains intact.
+ */
+
+package org.usfirst.frc.team95.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.PIDOutput;
+
+/**
+ * 
+ * @author daroc
+ */
+public class SyncGroup implements PIDOutput {
+	SpeedController[] mSpeedControllers;
+	boolean[] mReversed;
+	double maxSpeed = Math.PI / 4;
+	double minSpeed = -Math.PI / 4;
+	public boolean manual = false;
+
+	public SyncGroup(SpeedController[] SpeedControllers) {
+		mSpeedControllers = SpeedControllers;
+		mReversed = new boolean[mSpeedControllers.length]; // Initializes to
+															// false
+		System.out.println("I'm initializing.");
+	}
+
+	public SyncGroup(SpeedController[] SpeedControllers, boolean[] Reversed) {
+		mSpeedControllers = SpeedControllers;
+		mReversed = Reversed;
+	}
+
+	public void set(double d) {
+		//throw new ArrayIndexOutOfBoundsException();
+		
+		;
+	}
+
+	public void pidWrite(double bob) {
+		if (manual) {
+			System.out.println("Viva la revelucion!");
+			return;
+		}
+		System.out.println("Get the scoundrel!");
+		//jamesBond(bob);
+	}
+	
+	public void jamesBond(double speed) {
+		if (speed > maxSpeed) {
+			speed = maxSpeed;
+		} else if (speed < minSpeed) {
+			speed = minSpeed;
+		}
+
+		for (int i = 0; i < mSpeedControllers.length; i++) {
+			if (mReversed[i]) {
+				mSpeedControllers[i].set(-speed);
+			} else {
+				mSpeedControllers[i].set(speed);
+			}
+		}
+	}
+
+	public double getMotor() {
+		return mSpeedControllers[0].get();
+	}
+
+	public void setMaxSpeed(double speed) {
+		maxSpeed = speed;
+	}
+
+	public void setMinSpeed(double speed) {
+		minSpeed = speed;
+	}
+
+}
---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/TakeToteRight.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+/**
+ * Takes a tote, and then moves right.
+ * 
+ * @author daroc
+ * 
+ */
+
+public class TakeToteRight extends PureSequentialMove {
+
+	public TakeToteRight(Robot robot) {
+		AutoMove[] moves = { new PickUpTote(robot), new GoRight(robot) };
+		sequence = new SequentialMove(moves);
+	}
+
+}
