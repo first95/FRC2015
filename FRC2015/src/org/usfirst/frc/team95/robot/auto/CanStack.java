9e00a77037c3a19eeac95910ca3efc283af90079
Spencer <Spencer@localhost>
2015-02-14 13:07:09 -0500

    can stack finishes

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/CanStack.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/CanStack.java
@@ -4,27 +4,11 @@ import org.usfirst.frc.team95.robot.Robot;
 import org.usfirst.frc.team95.robot.RobotConstants;
 import edu.wpi.first.wpilibj.Timer;
 
-public class CanStack extends AutoMove {
+public class CanStack extends CanStackN {
 	Robot robot;
 	int stage = 0;
 
 	public CanStack(Robot robo) {
-		robot = robo;
-	}
-
-	@Override
-	public Status init() {
-		return null;
-	}
-
-	@Override
-	public Status periodic() {
-		return null;
-	}
-
-	@Override
-	public Status stop() {
-		// TODO Auto-generated method stub
-		return null;
+		super(robo,1);
 	}
 }
