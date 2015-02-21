42032a015d96893082498a91671b42ba2892e8e9
Spencer <Spencer@localhost>
2015-02-21 10:52:49 -0500

    added auto moves and lowered 90 turning timeout to 3 secends

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/TakeToteToAutoZone.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+public class TakeToteToAutoZone extends PureSequentialMove {
+	
+	public TakeToteToAutoZone(Robot robot){
+		AutoMove[] moves = { new PickUpTote(robot), new GoForward(robot) };
+		sequence = new SequentialMove(moves);
+	}
+}
