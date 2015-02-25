35db745d7b8efa9d55b243cf7821d03507118641
daroc <daroc@localhost>
2015-02-25 18:40:59 -0500

    IUcccdediui.,4ppppypv

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/PlainMotorMove.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
+
+public class PlainMotorMove extends AutoMove {
+	public double speed, time;
+	Timer timeOut;
+	SpeedController motor;
+	
+	public PlainMotorMove(SpeedController motor, double speed, double time) {
+		this.time = time;
+		this.speed = speed;
+		timeOut = new Timer();
+		timeOut.reset();
+		this.motor = motor;
+	}
+
+	@Override
+	public Status init() {
+		timeOut.reset();
+		timeOut.start();
+		return Status.wantsToContinue;
+	}
+
+	@Override
+	public Status periodic() {
+		if (timeOut.get() > time) {
+			return stop();
+		} else {
+			System.out.println("Plain outputing: " + speed);
+			motor.set(speed);
+			return Status.wantsToContinue;
+		}
+	}
+
+	@Override
+	public Status stop() {
+		timeOut.stop();
+		timeOut.reset();
+		return Status.isNotAbleToContinue;
+	}
+
+}
