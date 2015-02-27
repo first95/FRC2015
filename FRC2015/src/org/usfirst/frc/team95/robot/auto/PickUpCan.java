44b835bb5bc740b1b6c9bf77b68452718ab14da4
daroc <daroc@localhost>
2015-02-26 19:19:43 -0500

    Need to test auto moves.

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/PickUpCan.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/PickUpCan.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team95.robot.auto;
 
 import org.usfirst.frc.team95.robot.Robot;
-import org.usfirst.frc.team95.robot.RobotConstants;
 
 /**
  * Pick up a can from the set grabbing position.
@@ -10,36 +9,12 @@ import org.usfirst.frc.team95.robot.RobotConstants;
  * 
  */
 
-public class PickUpCan extends AutoMove {
+public class PickUpCan extends PureSequentialMove {
 	Robot robot;
 
 	public PickUpCan(Robot robo) {
-		robot = robo;
-	}
-
-	@Override
-	public Status init() {
-		robot.armController.setSetpoint(RobotConstants.kArmPositionGrab);
-		return Status.wantsToContinue;
-	}
-
-	@Override
-	public Status periodic() {
-		if (robot.armController.onTarget()
-				&& robot.armController.getSetpoint() == RobotConstants.kArmPositionGrab) {
-			robot.armPistons.set(true);
-			robot.armController.setSetpoint(RobotConstants.kArmPositionZenith);
-			return Status.needsToContinue;
-		} else if (robot.armController.onTarget()
-				&& robot.armController.getSetpoint() == RobotConstants.kArmPositionZenith) {
-			return Status.isNotAbleToContinue;
-		} else {
-			return Status.needsToContinue;
-		}
-	}
-
-	@Override
-	public Status stop() {
-		return Status.isNotAbleToContinue;
+		AutoMove[] stuff = {new TimedArmMove(robo, 1, 0.5), new Pistons(robo, true), 
+			new TimedArmMove(robo, -1, 0.5)};
+		sequence = new SequentialMove(stuff);
 	}
 }
