e6c8aa296e096d03f956e934319911b362f0a4dc
daroc <daroc@localhost>
2015-02-28 08:19:33 -0500

    More Auto Improvements.

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/QuickBarrierGrab.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.Robot;
+
+public class QuickBarrierGrab extends PureSequentialMove {
+	
+	public QuickBarrierGrab(Robot robot) {
+	AutoMove[] moves = { new PlainMotorMove(robot.armMotors, 0.5, 1.5),
+			new GoForward(robot, 1.0, 0.25), new Pistons(robot, true),
+			new PlainMotorMove(robot.armMotors, 0, 0.002),
+			new PlainMotorMove(robot.armMotors, -0.5, 0.5),
+			new PlainMotorMove(robot.armMotors, 0, 0.2),
+			new GoBackward(robot, 0.5, 0.25), new NoMove(robot) };
+	sequence = new SequentialMove(moves);
+	}
+
+}
