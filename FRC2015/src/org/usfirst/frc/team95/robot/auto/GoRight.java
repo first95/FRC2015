cf597e0220816e37274bf65b2b9c63ac9c052552
daroc <daroc@localhost>
2015-02-28 09:41:52 -0500

    Fixed sliding auto

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/GoRight.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GoRight.java
@@ -5,7 +5,7 @@ import org.usfirst.frc.team95.robot.Robot;
 public class GoRight extends PureSequentialMove {
 
 	public GoRight(Robot robot) {
-		AutoMove[] vector = { new MoveRelative(robot, 0.75, 0.0, 0.0, 1.5) };
+		AutoMove[] vector = { new MoveRelative(robot, -0.75, 0.0, 0.0, 0.5) };
 		sequence = new SequentialMove(vector);
 	}
 
