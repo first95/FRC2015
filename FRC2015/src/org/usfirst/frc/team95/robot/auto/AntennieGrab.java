89852b2a59a5810bc2fb682bfe55ff5d80867e77
daroc <daroc@localhost>
2015-03-07 09:11:57 -0500

    Fixed antennie.

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/AntennieGrab.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/AntennieGrab.java
@@ -5,7 +5,9 @@ import org.usfirst.frc.team95.robot.Robot;
 public class AntennieGrab extends PureSequentialMove {
 
 	public AntennieGrab(Robot robot) {
-		AutoMove[] moves = { new Pistons(robot.antennie, true), new GoBackward(robot),
+		AutoMove[] moves = { new PlainMotorMove(robot.armMotors, -0.5, 0.75), 
+				new Pistons(robot.antennie, true), 
+				new PlainMotorMove(robot.armMotors, -0.5, 0.75), new GoBackward(robot),
 				new Pistons(robot.antennie, false) };
 		sequence = new SequentialMove(moves);
 	}
