f7459af75b10b2f51587ef756cedfc3b6678ec29
daroc <daroc@localhost>
2015-03-12 18:35:49 -0400

    Fixed Rotate Grabber.

--- FRC2015/src/org/usfirst/frc/team95/robot/auto/GrabCanAndFlip.java
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GrabCanAndFlip.java
@@ -6,7 +6,7 @@ public class GrabCanAndFlip extends PureSequentialMove{
 	Robot robot;
 
 	public GrabCanAndFlip(Robot robo) {
-		AutoMove[] stuff = { new PickUpCan(robo), new RotateGrabber(robo)};
+		AutoMove[] stuff = { new PickUpCan(robo), new Pistons(robo.grabberRotatePiston, true)};
 		sequence = new SequentialMove(stuff);
 
 }}
