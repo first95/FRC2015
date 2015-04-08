792e044ca432408ffcb06fe719cec91ef386d554
daroc <daroc@localhost>
2015-04-08 19:29:51 -0400

    Added smashing top-pin on Jame's request.

---
+++ FRC2015/src/org/usfirst/frc/team95/robot/auto/GoExactlyForward.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team95.robot.auto;
+
+import org.usfirst.frc.team95.robot.ResetableGyro;
+import org.usfirst.frc.team95.robot.Robot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+
+public class GoExactlyForward extends AutoMove {
+	RobotDrive drive;
+	ResetableGyro gyro;
+	Timer timeOut;
+	double timeLimit;
+	
+	public GoExactlyForward(Robot robot, double time) {
+		drive = robot.driveTrain;
+		gyro = robot.gyro;
+		timeOut = new Timer();
+		timeLimit = time;
+	}
+	
+	public Status init() {
+		timeOut.reset();
+		timeOut.start();
+		return Status.wantsToContinue;
+	}
+	
+	public Status periodic() {
+		if (timeOut.get() > timeLimit) {
+			return Status.isAbleToContinue;
+		} else {
+			drive.mecanumDrive_Cartesian(0, 0.75, gyro.getAngle(), 2);
+		}
+		return Status.wantsToContinue;
+	}
+	
+	public Status stop() {
+		return Status.isNotAbleToContinue;
+	}
+
+}
